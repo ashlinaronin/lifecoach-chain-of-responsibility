fbc6629a1a053f40093f9b0ec76519982ac9d7fd
MitchRivet <mitch.rivet@gmail.com>
2015-08-31 16:48:55 -0700

    add crud functionality for habit class with completed boolean property

--- src/Habit.php
+++ src/Habit.php
@@ -5,13 +5,15 @@
       private $name;
       private $motivation;
       private $interval_days;
+      private $completed;
       private $id;
 
-      function __construct($name, $motivation, $interval_days, $id = null)
+      function __construct($name, $motivation, $interval_days, $completed, $id = null)
       {
         $this->name = $name;
         $this->motivation = $motivation;
         $this->interval_days = $interval_days;
+        $this->completed = (int) $completed;
         $this->id = $id;
       }
 
@@ -24,6 +26,109 @@
       {
         $this->name = $new_name;
       }
+
+      function getMotivation()
+      {
+          return $this->motivation;
+      }
+
+      function setMotivation($new_motivation)
+      {
+          $this->motivation = $new_motivation;
+      }
+
+      function getIntervalDays()
+      {
+          return $this->interval_days;
+      }
+
+      function setIntervalDays($new_interval_days)
+      {
+          $this->interval_days = $new_interval_days;
+      }
+
+      function getCompleted()
+      {
+          return $this->completed;
+      }
+
+      function setCompleted($new_completed)
+      {
+          $this->completed = (int) $new_completed;
+      }
+
+      function getId()
+      {
+          return $this->id;
+      }
+
+      function save()
+      {
+          $GLOBALS['DB']->exec("INSERT INTO habits (name, motivation, interval_days, completed)
+          VALUES ('{$this->getName()}', '{$this->getMotivation()}', {$this->getIntervalDays()}, {$this->getCompleted()});");
+          $this->id = $GLOBALS['DB']->lastInsertId();
+      }
+
+      static function getAll()
+      {
+          $returned_habits = $GLOBALS['DB']->query("SELECT * FROM habits;");
+
+          $habits = array();
+          foreach($returned_habits as $habit) {
+              $habit_name = $habit['name'];
+              $habit_motivation = $habit['motivation'];
+              $habit_interval_days = $habit['interval_days'];
+              $habit_completed = $habit['completed'];
+              $habit_id = $habit['id'];
+              $new_habit = new Habit($habit_name, $habit_motivation, $habit_interval_days, $habit_completed, $habit_id);
+              array_push($habits, $new_habit);
+          }
+
+         return $habits;
+      }
+
+      static function deleteAll()
+      {
+          $GLOBALS['DB']->exec("DELETE FROM habits;");
+      }
+
+      static function find($search_id)
+      {
+          $found_habit = null;
+          $habits = Habit::getAll();
+          foreach($habits as $habit) {
+              $habit_id = $habit->getId();
+              if ($habit_id == $search_id) {
+                  $found_habit = $habit;
+              }
+          }
+
+          return $found_habit;
+      }
+
+      function updateName($new_habit_name)
+      {
+          $GLOBALS['DB']->exec("UPDATE habits SET name = '{$new_habit_name}' WHERE id = {$this->getId()};");
+          $this->setName($new_habit_name);
+      }
+
+      function updateMotivation($new_habit_motivation)
+      {
+          $GLOBALS['DB']->exec("UPDATE habits SET motivation = '{$new_habit_motivation}' WHERE id = {$this->getId()};");
+          $this->setMotivation($new_habit_motivation);
+      }
+
+      function updateIntervalDays($new_habit_interval_days)
+      {
+          $GLOBALS['DB']->exec("UPDATE habits SET interval_days = {$new_habit_interval_days} WHERE id = {$this->getId()};");
+          $this->setIntervalDays($new_habit_interval_days);
+      }
+
+      function delete()
+      {
+          $GLOBALS['DB']->exec("DELETE FROM habits WHERE id = {$this->getId()};");
+      }
+
 
     }
 
--- tests/HabitTest.php
+++ tests/HabitTest.php
@@ -10,15 +10,18 @@
     $DB = new PDO($server, $username, $password);
     class HabitTest extends PHPUnit_Framework_TestCase
     {
-
+        protected function tearDown()
+        {
+            Habit::deleteAll();
+        }
         function testGetName()
         {
             $name = "Meditate";
             $motivation = "Clarity";
             $interval_days = 3;
+            $completed = False;
 
-
-            $test_habit = new Habit($name, $motivation, $interval_days);
+            $test_habit = new Habit($name, $motivation, $interval_days, $completed);
             $result = $test_habit->getName();
 
             $this->assertEquals($name, $result);
@@ -30,11 +33,247 @@
             $name = "Meditate";
             $motivation = "Clarity";
             $interval_days = 3;
+            $completed = False;
 
-            $test_habit = new Habit($name, $motivation, $interval_days);
+            $test_habit = new Habit($name, $motivation, $interval_days, $completed);
             $result = $test_habit->getName();
 
             $this->assertEquals("Meditate", $result);
+        }
+
+        function testGetMotivation()
+        {
+            $name = "Meditate";
+            $motivation = "Clarity";
+            $interval_days = 3;
+            $completed = False;
+
+            $test_habit = new Habit($name, $motivation, $interval_days, $completed);
+            $result = $test_habit->getMotivation();
+
+            $this->assertEquals($motivation, $result);
+
+        }
+
+        function testSetMotivation()
+        {
+            $name = "Meditate";
+            $motivation = "Clarity";
+            $interval_days = 3;
+            $completed = False;
+
+            $test_habit = new Habit($name, $motivation, $interval_days, $completed);
+            $result = $test_habit->getMotivation();
+
+            $this->assertEquals("Clarity", $result);
+        }
+
+        function testGetIntervalDays()
+        {
+            $name = "Meditate";
+            $motivation = "Clarity";
+            $interval_days = 3;
+            $completed = False;
+
+
+            $test_habit = new Habit($name, $motivation, $interval_days, $completed);
+            $result = $test_habit->getIntervalDays();
+
+            $this->assertEquals($interval_days, $result);
+
+        }
+
+        function testSetIntervalDays()
+        {
+            $name = "Meditate";
+            $motivation = "Clarity";
+            $interval_days = 3;
+            $completed = False;
+
+            $test_habit = new Habit($name, $motivation, $interval_days, $completed);
+            $result = $test_habit->getIntervalDays();
+
+            $this->assertEquals(3, $result);
+        }
+
+        function testGetId()
+        {
+            $name = "Meditate";
+            $motivation = "Clarity";
+            $interval_days = 3;
+            $id = 4;
+            $completed = False;
+            $test_habit = new Habit($name, $motivation, $interval_days, $completed, $id);
+
+            $result = $test_habit->getId();
+
+            $this->assertEquals(4, $result);
+        }
+
+        function testSave()
+        {
+            $name = "Meditate";
+            $motivation = "Clarity";
+            $interval_days = 3;
+            $completed = false;
+            $test_habit = new Habit($name, $motivation, $interval_days, $completed);
+
+            var_dump($test_habit);
+
+            $test_habit->save();
+
+            $this->assertEquals(is_numeric($test_habit->getId()), true);
+
+
+        }
+
+        function testGetAll()
+        {
+            $name = "Meditate";
+            $motivation = "Clarity";
+            $interval_days = 3;
+            $completed = False;
+            $test_habit = new Habit($name, $motivation, $interval_days, $completed);
+            $test_habit->save();
+
+            $name2 = "Go running";
+            $motivation2 = "Fitness";
+            $interval_days2 = 35;
+            $completed2 = true;
+            $test_habit2 = new Habit($name2, $motivation2, $interval_days2, $completed2);
+            $test_habit2->save();
+
+
+
+            $result = Habit::getAll();
+
+            var_dump($result);
+
+            $this->assertEquals([$test_habit, $test_habit2], $result);
+
+        }
+        
+        function testDeleteAll()
+        {
+            $name = "Meditate";
+            $motivation = "Clarity";
+            $interval_days = 3;
+            $id = 4;
+            $completed = False;
+            $test_habit = new Habit($name, $motivation, $interval_days, $completed, $id);
+            $test_habit->save();
+
+            $name2 = "Go running";
+            $motivation2 = "Fitness";
+            $interval_days2 = 35;
+            $id2 = 5;
+            $completed2 = False;
+            $test_habit2 = new Habit($name2, $motivation2, $interval_days2, $completed2, $id2);
+            $test_habit2->save();
+
+            Habit::deleteAll();
+            $result = Habit::getAll();
+
+            $this->assertEquals([], $result);
+        }
+
+        function testFind()
+        {
+            $name = "Meditate";
+            $motivation = "Clarity";
+            $interval_days = 3;
+            $id = 4;
+            $completed = False;
+            $test_habit = new Habit($name, $motivation, $interval_days, $completed, $id);
+            $test_habit->save();
+
+            $name2 = "Go running";
+            $motivation2 = "Fitness";
+            $interval_days2 = 35;
+            $id2 = 5;
+            $completed2 = False;
+            $test_habit2 = new Habit($name2, $motivation2, $interval_days2, $completed2, $id2);
+            $test_habit2->save();
+
+            $result = Habit::find($test_habit->getId());
+
+            $this->assertEquals($test_habit, $result);
+        }
+
+        function testUpdateName()
+        {
+            $name = "Meditate";
+            $motivation = "Clarity";
+            $interval_days = 3;
+            $id = 4;
+            $completed = False;
+            $test_habit = new Habit($name, $motivation, $interval_days, $completed, $id);
+            $test_habit->save();
+
+            $new_name = "Dance";
+
+            $result = $test_habit->updateName($new_name);
+
+            $this->assertEquals("Dance", $test_habit->getName());
+        }
+
+        function testUpdateMotivation()
+        {
+            $name = "Meditate";
+            $motivation = "Clarity";
+            $interval_days = 3;
+            $id = 4;
+            $completed = False;
+            $test_habit = new Habit($name, $motivation, $interval_days, $completed, $id);
+            $test_habit->save();
+
+            $new_motivation = "Hate my dad";
+
+            $result = $test_habit->updateMotivation($new_motivation);
+
+            $this->assertEquals("Hate my dad", $test_habit->getMotivation());
+        }
+
+        function testUpdateIntervalDays()
+        {
+            $name = "Meditate";
+            $motivation = "Clarity";
+            $interval_days = 3;
+            $id = 4;
+            $completed = False;
+            $test_habit = new Habit($name, $motivation, $interval_days, $completed, $id);
+            $test_habit->save();
+
+            $new_interval_days = 12;
+
+            $result = $test_habit->updateIntervalDays($new_interval_days);
+
+            $this->assertEquals(12, $test_habit->getIntervalDays());
+        }
+
+        function testDelete()
+        {
+            $name = "Meditate";
+            $motivation = "Clarity";
+            $interval_days = 3;
+            $id = 4;
+            $completed = False;
+            $test_habit = new Habit($name, $motivation, $interval_days, $completed, $id);
+            $test_habit->save();
+
+            $name2 = "Go running";
+            $motivation2 = "Fitness";
+            $interval_days2 = 35;
+            $id2 = 5;
+            $completed2 = False;
+            $test_habit2 = new Habit($name2, $motivation2, $interval_days2, $completed2, $id2);
+            $test_habit2->save();
+
+            $test_habit->delete();
+
+            $this->assertEquals([$test_habit2], Habit::getAll());
+
+
         }
     }
 ?>
